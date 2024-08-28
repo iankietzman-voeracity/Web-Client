322e84d33e419b53f53d71e10f8647aa148be972
iankietzman-voeracity <ian+voeracity@tallgrassschool.com>
2024-08-28 18:28:07 -0500

    adds / to deployment base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: 'voeracity',
+    base: '/voeracity',
     test: {
         globals: true,
         environment: 'jsdom',
