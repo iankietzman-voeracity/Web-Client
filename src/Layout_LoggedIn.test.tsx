c60f5a13fb13f5640cf38b246226ab4597945082
iankietzman-voeracity <ian+voeracity@tallgrassschool.com>
2024-08-28 18:09:49 -0500

    fixes TS error with typecasting

--- src/Layout_LoggedIn.test.tsx
+++ src/Layout_LoggedIn.test.tsx
@@ -36,10 +36,9 @@ describe('Layout - Logged in', () => {
     it('renders the logged in Layout component view', async () => {
         const nav = screen.getAllByRole('link')
         const navTextArray = nav.map((navItem) => {
-            return navItem.text
+            return (navItem as HTMLAnchorElement).text
         })
-        console.log('nta', navTextArray);
-        
+
         expect(navTextArray.find((i) => i == 'Home')).toBeTruthy()
         expect(navTextArray.find((i) => i == 'Account')).toBeTruthy()
         expect(navTextArray.find((i) => i == 'Logout')).toBeTruthy()
